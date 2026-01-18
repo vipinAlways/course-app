568246bb8df418d0546313994361d9931e636507
vipinAlways <vipinkumarti555@gmail.com>
2026-01-18 23:37:52 +0530

    arrenging acc to categories

--- src/app/(app)/courses/page.tsx
+++ src/app/(app)/courses/page.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import CourseCard from '~/components/CourseCard'
+import React from "react";
+import CourseCard from "~/components/CourseCard";
 
 const page = () => {
+  const cards: CourseCardData[] = [
+    {
+      category: "",
+      creator: "",
+      description: "",
+      id: "",
+      image: "",
+      price: 122,
+      title: "",
+    },
+  ];
   return (
-    <div >
-      
-      <CourseCard cardData={{
-        category:"ds",
-        creator:"mee eee",
-        description:"dssssssss",
-        image:"https://imgs.search.brave.com/CqYd0Sa4ritZQkWrX4njLZ_CgtrZu87qjPrDmvP4WzQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9maXZl/cnItcmVzLmNsb3Vk/aW5hcnkuY29tL2lt/YWdlcy90X21haW4x/LHFfYXV0byxmX2F1/dG8scV9hdXRvLGZf/YXV0by9naWdzLzMy/MzQwNDEyNS9vcmln/aW5hbC85MjQzZjU0/ZDIzNWU5NzlkYjI0/ZGFmYzQwODBjNTFh/MWM2MzgyNWMzL2Rl/c2lnbi1vbmxpbmUt/Y291cnNlLXRodW1i/bmFpbC11ZGVteS1j/b3Vyc2UtY292ZXIt/aW1hZ2UucG5n",
-        price:300,
-        title:"start today"
+    <div>
+      {cards.map((card, index) => (
+        <div key={index}>
+          <h1>{card.category}</h1>
 
-      }}/>
+          <CourseCard cardData={card} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
