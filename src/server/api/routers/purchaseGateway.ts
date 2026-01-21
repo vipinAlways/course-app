cecfade0b495ba18556ea23ca7ff6a1462bbb8ee
vipinAlways <vipinkumarti555@gmail.com>
2026-01-21 23:29:17 +0530

    paymentintent model and docker setup

--- src/server/api/routers/purchaseGateway.ts
+++ src/server/api/routers/purchaseGateway.ts
@@ -1,5 +1,23 @@
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 const purchageGateway = createTRPCRouter({
-  buyCourse: protectedProcedure.mutation(async () => {}),
+  buyCourse: protectedProcedure.mutation(async ({ ctx }) => {
+    const user = await ctx.db.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+   
+    });
+
+    if(!user){
+      throw new TRPCError({
+        cause:user,
+        code:"NOT_FOUND",
+        message:"User not found"
+      })
+    }
+
+  
+  }),
 });
